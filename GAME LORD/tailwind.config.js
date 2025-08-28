af3bdf71f7ed6c039c624f6474502cbcb6e6bcd7
sadeqsoli <solisadeq@gmail.com>
2025-08-28 11:20:13 +0330

    installing tailwind and new packages.

---
+++ GAME LORD/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./popup.html",
+    "./dino.html",
+    "./pong.html",
+    "./snake.html",
+    "./src/**/*.{ts,tsx,js,jsx,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
